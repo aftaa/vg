00bbf2a34a244e362cf2c92e66957d1fd03904c6
Maxim Gabidullin <after@ya.ru>
2019-06-20 04:57:47 +0300

    create company category migration

---
+++ console/migrations/m190620_014546_create_company_category_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%company_category}}`.
+ */
+class m190620_014546_create_company_category_table extends Migration
+{
+    const TABLE_NAME = 'company_category';
+    const IDX_PARENT_ID = 'idx-parent-id';
+    const PARENT_ID_COLUMN = 'parent_id';
+    const FK_PARENT_ID = 'fk-parent-id';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%company_category}}', [
+            'id'                   => $this->primaryKey()->comment('№'),
+            self::PARENT_ID_COLUMN => $this->integer()->comment('Родительская категория'),
+            'name'                 => $this->string(150)->notNull()->comment('Категория'),
+            'sort'                 => $this->integer()->unsigned()->notNull()->defaultValue(0)->comment('Порядок'),
+            'icon'                 => $this->string(30)->null()->comment('Иконка'),
+            'meta_keywords'        => $this->text()->null()->comment('Meta Keywords'),
+            'meta_description'     => $this->text()->null()->comment('Meta Description'),
+        ]);
+
+        $this->createIndex(self::IDX_PARENT_ID, self::TABLE_NAME, self::PARENT_ID_COLUMN);
+        $this->addForeignKey(self::FK_PARENT_ID, self::TABLE_NAME, self::PARENT_ID_COLUMN, self::TABLE_NAME, 'id');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropForeignKey(self::FK_PARENT_ID, self::TABLE_NAME);
+        $this->dropIndex(self::IDX_PARENT_ID, self::TABLE_NAME);
+        $this->dropTable('{{%company_category}}');
+    }
+}
