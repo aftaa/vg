ca21dbf614805bfeaba1f14992f03fccb0b39101
Maxim Gabidullin <after@ya.ru>
2019-09-10 20:27:30 +0300

    moved local params to main

--- common/config/main.php
+++ common/config/main.php
@@ -1,5 +1,6 @@
 <?php
 return [
+    'name' => 'В сети города',
     'aliases' => [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
@@ -9,6 +10,41 @@ return [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
+        'db'              => [
+            'class'    => 'yii\db\Connection',
+            'dsn'      => 'mysql:host=localhost;dbname=vg',
+            'username' => 'vg',
+            'password' => 'vg',
+            'charset'  => 'utf8',
+        ],
+        'dbVsetigTest'    => [
+            'class'    => 'yii\db\Connection',
+            'dsn'      => 'mysql:host=vsetig.beget.tech;dbname=vsetig_test',
+            'username' => 'vsetig_test',
+            'password' => 'DarkS1de423',
+            'charset'  => 'utf8',
+        ],
+        'dbVsetigInfoCom' => [
+            'class'    => 'yii\db\Connection',
+            'dsn'      => 'mysql:host=vsetig.beget.tech;dbname=vsetig_info_com',
+            'username' => 'vsetig_info_com',
+            'password' => 'DarkS1de423',
+            'charset'  => 'utf8',
+        ],
+        'dbVsetigCat'     => [
+            'class'    => 'yii\db\Connection',
+            'dsn'      => 'mysql:host=vsetig.beget.tech;dbname=vsetig_cat',
+            'username' => 'vsetig_cat',
+            'password' => 'DarkS1de423',
+            'charset'  => 'utf8',
+        ],
+        'mailer' => [
+            'class'            => 'yii\swiftmailer\Mailer',
+            'viewPath'         => '@common/mail',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => true,
+        ],
     ],
-    'name' => 'В сети города',
 ];
