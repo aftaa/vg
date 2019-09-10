a96e7692908be074dd789ec09e6748de1fe884f1
Maxim Gabidullin <after@ya.ru>
2019-09-10 15:55:54 +0300

    after issue report

--- common/config/params.php
+++ common/config/params.php
@@ -5,7 +5,7 @@ return [
         'after@ya.ru',
     ],
     'supportEmail' => 'support@vseti-goroda.ru',
-    'senderEmail' => 'noreply@vseti-goroda.ru',
-    'senderName' => 'Каталог vseti-goroda.ru',
+    'senderEmail' => 'admin@vseti-goroda.ru',
+    'senderName' => 'Каталог «В сети города»',
     'user.passwordResetTokenExpire' => 3600,
 ];
