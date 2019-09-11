5d060d7116ee58ab933b8665e26e2736dfbdca95
Maxim Gabidullin <after@ya.ru>
2019-09-12 01:54:09 +0300

    console application

--- common/config/main.php
+++ common/config/main.php
@@ -1,44 +1,74 @@
 <?php
 return [
-    'name' => 'В сети города',
-    'aliases' => [
+    'name'       => 'В сети города',
+    'aliases'    => [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
     ],
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
-        'cache' => [
+        'cache'  => [
             'class' => 'yii\caching\FileCache',
         ],
-        'db'              => [
+        'db'     => [
             'class'    => 'yii\db\Connection',
             'dsn'      => 'mysql:host=localhost;dbname=vg',
             'username' => 'vg',
             'password' => 'vg',
             'charset'  => 'utf8',
         ],
+
+        'dbSource' => [
+            'class'    => 'yii\db\Connection',
+            'dsn'      => 'mysql:host=localhost;dbname=vsetig',
+            'username' => 'vg',
+            'password' => 'vg',
+            'charset'  => 'utf8',
+        ],
+
         'dbVsetigTest'    => [
             'class'    => 'yii\db\Connection',
-            'dsn'      => 'mysql:host=vsetig.beget.tech;dbname=vsetig_test',
-            'username' => 'vsetig_test',
-            'password' => 'DarkS1de423',
+            'dsn'      => 'mysql:host=localhost;dbname=vg',
+            'username' => 'vg',
+            'password' => 'vg',
             'charset'  => 'utf8',
         ],
         'dbVsetigInfoCom' => [
             'class'    => 'yii\db\Connection',
-            'dsn'      => 'mysql:host=vsetig.beget.tech;dbname=vsetig_info_com',
-            'username' => 'vsetig_info_com',
-            'password' => 'DarkS1de423',
+            'dsn'      => 'mysql:host=localhost;dbname=vg',
+            'username' => 'vg',
+            'password' => 'vg',
             'charset'  => 'utf8',
         ],
         'dbVsetigCat'     => [
             'class'    => 'yii\db\Connection',
-            'dsn'      => 'mysql:host=vsetig.beget.tech;dbname=vsetig_cat',
-            'username' => 'vsetig_cat',
-            'password' => 'DarkS1de423',
+            'dsn'      => 'mysql:host=localhost;dbname=vg',
+            'username' => 'vg',
+            'password' => 'vg',
             'charset'  => 'utf8',
         ],
-        'mailer' => [
+//        'dbVsetigTest'    => [
+//            'class'    => 'yii\db\Connection',
+//            'dsn'      => 'mysql:host=vsetig.beget.tech;dbname=vsetig_test',
+//            'username' => 'vsetig_test',
+//            'password' => 'DarkS1de423',
+//            'charset'  => 'utf8',
+//        ],
+//        'dbVsetigInfoCom' => [
+//            'class'    => 'yii\db\Connection',
+//            'dsn'      => 'mysql:host=vsetig.beget.tech;dbname=vsetig_info_com',
+//            'username' => 'vsetig_info_com',
+//            'password' => 'DarkS1de423',
+//            'charset'  => 'utf8',
+//        ],
+//        'dbVsetigCat'     => [
+//            'class'    => 'yii\db\Connection',
+//            'dsn'      => 'mysql:host=vsetig.beget.tech;dbname=vsetig_cat',
+//            'username' => 'vsetig_cat',
+//            'password' => 'DarkS1de423',
+//            'charset'  => 'utf8',
+//        ],
+        'mailer'          => [
             'class'            => 'yii\swiftmailer\Mailer',
             'viewPath'         => '@common/mail',
             // send all mails to a file by default. You have to set
---
+++ console/controllers/vg/db/TransferController.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace console\controllers\vg\db;
+
+use Yii;
+use yii\base\InvalidConfigException;
+use yii\console\Controller;
+use yii\console\controllers\BaseMigrateController;
+use yii\db\Connection;
+use yii\db\Exception;
+
+class TransferController extends Controller
+{
+    const MIGRATE_TABLE_NAME = 'migrate';
+
+    /**
+     * @return int
+     * @throws InvalidConfigException
+     */
+    public function actionIndex()
+    {
+        try {
+            $db = Yii::$app->getDb();
+            $this->prepareDatabase($db);
+
+            /** @var Connection $sbSource */
+            $dbSource = Yii::$app->dbSource;
+
+
+            $vsetigDatabases = [
+                'dbVsetigTest',
+                'dbVsetigInfoCom',
+                'dbVsetigCat'
+            ];
+
+            foreach ($vsetigDatabases as $databaseName) {
+                /** @var Connection $vsetigDb */
+                $vsetigDb = Yii::$app->get($databaseName);
+                $sourceDbName = str_replace('mysql:host=vsetig.beget.tech;dbname=', '', $vsetigDb->dsn);
+
+                $vsetigTableNames = $vsetigDb->createCommand('SHOW TABLES')->queryColumn();
+
+                echo "\nБаза данных: $databaseName.\n\n";
+
+
+                foreach ($vsetigTableNames as $sourceTableName) {
+//                    echo "$sourceTableName ";
+
+                    $dbSource
+                        ->createCommand("CREATE TABLE vsetig.$sourceTableName LIKE $sourceDbName.$sourceTableName")
+                        ->execute();
+
+                }
+                echo "\n\n";
+            }
+
+
+            return 1;
+
+        } catch (Exception $e) {
+            echo "{$e->getMessage()}\n.";
+            return 0;
+        }
+    }
+
+    /**
+     * @param Connection $db
+     * @throws Exception
+     */
+    private function foreignKeyChecksOff(Connection $db): void
+    {
+        $db->createCommand('SET foreign_key_checks = 0')->execute();
+        echo "Проверка ссылочной целостности отключена.\n";
+    }
+
+    /**
+     * @param Connection $db
+     * @throws Exception
+     */
+    private function foreignKeyChecksOn(Connection $db): void
+    {
+        $db->createCommand('SET foreign_key_checks = 1')->execute();
+        echo "Проверка ссылочной целостности включена.\n";
+    }
+
+    /**
+     * @param Connection $db
+     * @throws Exception
+     */
+    private function prepareDatabase(Connection $db): void
+    {
+        $this->foreignKeyChecksOff($db);
+        $tables = $this->getTableNames($db);
+        foreach ($tables as $table) {
+            if ($this->isMigrateTable($table)) {
+                $this->cleanMigrateTable($db);
+            } else {
+                $this->dropTable($db, $table);
+            }
+        }
+        $this->foreignKeyChecksOn($db);
+    }
+
+    /**
+     * @param Connection $db
+     * @return array
+     * @throws Exception
+     */
+    private function getTableNames(Connection $db): array
+    {
+        return $db->createCommand('SHOW TABLES')->queryColumn();
+    }
+
+    /**
+     * @param Connection $db
+     */
+    private function cleanMigrateTable(Connection $db): void
+    {
+        $baseMigrationVersion = BaseMigrateController::BASE_MIGRATION;
+        $db->createCommand("DELETE FROM migrate WHERE baseMigrationVersion <> '$baseMigrationVersion'");
+        $migrateTableName = self::MIGRATE_TABLE_NAME;
+        echo "Таблица $migrateTableName очищена.\n";
+    }
+
+    /**
+     * @param Connection $db
+     * @param $table
+     * @throws Exception
+     */
+    private function dropTable(Connection $db, $table): void
+    {
+        $db->createCommand("DROP TABLE $table")->execute();
+        echo "Таблица $table удалена.\n";
+    }
+
+    /**
+     * @return bool
+     */
+    private function isMigrateTable(): bool
+    {
+        return self::MIGRATE_TABLE_NAME == $table;
+    }
+}
