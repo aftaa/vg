18fee15149a826d2a65c7995b2034813b8b7dde7
Maxim Gabidullin <after@ya.ru>
2019-09-20 23:35:15 +0300

    console/controllers/vg/product/ test

---
+++ console/controllers/vg/product/ThumbController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace console\controllers\vg\product;
+
+use common\models\Product;
+use yii\console\Controller;
+
+class ThumbController extends Controller
+{
+
+
+    public function actionIndex()
+    {
+        echo "Usage:\n";
+        echo "1. vg/product/check\n";
+        echo "2. vg/product/correct\n"; // relative path 2 absolute
+        echo "3. vg/product/setnull\n"; // image is missing
+        exit(0);
+    }
+
+    public function actionCheck()
+    {
+        $time = time();
+
+        $products = Product::find()
+            ->select('id, thumb')
+            ->where('thumb IS NOT NULL')
+            ->indexBy('id')
+            ->limit(10)
+        ;
+//            ->all();
+
+        $results = [];
+
+        $i = 1;
+        foreach ($products->each(100) as $id => $product) {
+
+            $url = $product->thumb;
+            $url = $this->correctUrl($url);
+
+            if (null === $url) {
+                Product::updateAll(['thumb' => null], "id=$id");
+                continue;
+            }
+
+            if ($url) {
+                $ch = curl_init($url);
+                curl_setopt($ch, CURLOPT_MAXREDIRS, 10);
+                curl_setopt($ch, CURLOPT_FRESH_CONNECT, true);
+                curl_setopt($ch, CURLOPT_HEADER, true);
+                curl_setopt($ch, CURLOPT_POST, false);
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+                curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+                curl_setopt($ch, CURLOPT_DEFAULT_PROTOCOL, 'http');
+
+                $response = curl_exec($ch);
+
+                if (false === $response) {
+                    $error = curl_error($ch);
+                    $results['error'][] = $error . ": URL='$url'";
+                    echo "$url: $error\n";
+
+                    Product::updateAll(['thumb' => null], "id=$id");
+
+                } else {
+                    $httpResponseCode = curl_getinfo($ch, CURLINFO_RESPONSE_CODE);
+                    $results[$httpResponseCode]++;
+                    echo "$url: $httpResponseCode\n";
+                }
+                curl_close($ch);
+            }
+            $i++;
+        }
+        print_r($results);
+
+        echo "\nTime: ", round((time() - $time) / 60, 2), " min.\n";
+    }
+
+    /**
+     * @param string $url
+     * @return string|null
+     */
+    private function correctUrl(string $url)
+    {
+        if ('' == trim($url)) {
+            return null;
+        }
+
+        if (!preg_match('/^https?/i', $url)) {
+            if ('/' == $url[0]) {
+                $url = "http://vseti-goroda.ru$url";
+            } else {
+                $url = "http://vseti-goroda.ru/$url";
+            }
+        }
+        return $url;
+    }
+}
