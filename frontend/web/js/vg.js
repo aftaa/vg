1417fddec385c8e2e4327e7d90bea543db7a5f53
Maxim Gabidullin <after@ya.ru>
2019-09-20 04:27:46 +0300

    confirm user agreement, corrections

--- frontend/web/js/vg.js
+++ frontend/web/js/vg.js
@@ -1,33 +1,3 @@
-if (false) {
-
-    function lockScreen() {
-
-        $('input,select,textarea,button').css({disabled: true});
-        $('a').prop('href', '#');
-        $('*').css({cursor: 'wait'});
-        $('div.wrap,footer').fadeOut('slow');
-    }
-
-    $('form').submit(function () {
-        lockScreen();
-        return true;
-    });
-
-    $('a').click(function (e) {
-        e.preventDefault();
-        lockScreen();
-        return true;
-    });
-
-    $(function () {
-        $(document).load(function () {
-            $('div.wrap,footer').fadeIn('fast');
-        })
-    });
-
-}
-
-
 $(function () {
     // index page blocks
     let animateEnabled = false;
