6e79d7d2a319b149b40bf30a48ec5d1b0bacbbce
Maxim Gabidullin <gabidullin@gmail.com>
2019-11-08 11:59:55 +0300

    fix up db connections in main/local config files

--- common/config/main.php
+++ common/config/main.php
@@ -1,21 +1,21 @@
 <?php
 return [
-    'name'       => 'В сети города',
-    'language'   => 'ru-RU',
-    'aliases'    => [
+    'name' => 'В сети города',
+    'language' => 'ru-RU',
+    'aliases' => [
         '@bower' => '@vendor/bower-asset',
-        '@npm'   => '@vendor/npm-asset',
+        '@npm' => '@vendor/npm-asset',
     ],
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
-        'i18n'  => [
+        'i18n' => [
             'translations' => [
                 '*' => [
-                    'class'          => 'yii\i18n\PhpMessageSource',
-                    'basePath'       => '@app/messages',
+                    'class' => 'yii\i18n\PhpMessageSource',
+                    'basePath' => '@app/messages',
                     'sourceLanguage' => 'en-US',
-                    'fileMap'        => [
-                        'app'       => 'app.php',
+                    'fileMap' => [
+                        'app' => 'app.php',
                         'app/error' => 'error.php',
                     ],
                 ],
@@ -24,45 +24,17 @@ return [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
-        'db'    => [
-            'class'    => 'yii\db\Connection',
-            'dsn'      => 'mysql:host=localhost;dbname=vg_dev',
-            'username' => 'vg',
-            'password' => 'vg',
-            'charset'  => 'utf8',
-        ],
-        'dbVsetigTest'    => [
-            'class'    => 'yii\db\Connection',
-            'dsn'      => 'mysql:host=localhost;dbname=vsetig',
-            'username' => 'vsetig',
-            'password' => 'vsetig',
-            'charset'  => 'utf8',
-        ],
-        'dbVsetigInfoCom' => [
-            'class'    => 'yii\db\Connection',
-            'dsn'      => 'mysql:host=localhost;dbname=vsetig',
-            'username' => 'vsetig',
-            'password' => 'vsetig',
-            'charset'  => 'utf8',
-        ],
-        'dbVsetigCat'     => [
-            'class'    => 'yii\db\Connection',
-            'dsn'      => 'mysql:host=localhost;dbname=vsetig',
-            'username' => 'vsetig',
-            'password' => 'vsetig',
-            'charset'  => 'utf8',
-        ],
-        'mailer'          => [
-            'class'    => 'yii\swiftmailer\Mailer',
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
             'viewPath' => '@common/mail',
             // send all mails to a file by default. You have to set
             // 'useFileTransport' to false and configure a transport
             // for the mailer to send real emails.
             // 'useFileTransport' => true,
         ],
-        'sphinx'          => [
-            'class'    => 'yii\sphinx\Connection',
-            'dsn'      => 'mysql:host=127.0.0.1;port=9306;',
+        'sphinx' => [
+            'class' => 'yii\sphinx\Connection',
+            'dsn' => 'mysql:host=127.0.0.1;port=9306;',
             'username' => '',
             'password' => '',
         ],
