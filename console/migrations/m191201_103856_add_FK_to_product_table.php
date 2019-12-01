f14855944c6a47128d1195f00261a7a171278f3d
Maxim Gabidullin <gabidullin@gmail.com>
2019-12-01 14:26:17 +0300

    блять

--- console/migrations/m191201_103856_add_FK_to_product_table.php
+++ console/migrations/m191201_103856_add_FK_to_product_table.php
@@ -12,13 +12,10 @@ class m191201_103856_add_FK_to_product_table extends Migration
      */
     public function safeUp()
     {
-        $this->dropForeignKey('fk-product-company', 'product');
-
         Yii::$app->db->createCommand('SET foreign_key_checks = 0')->execute();
         $this->addForeignKey('fk-product-company', 'product', 'company_id',
             'company', 'id', 'SET NULL');
 
-        Yii::$app->db->createCommand('SET foreign_key_checks = 0')->execute();
         $this->addForeignKey('fk-product-product-category', 'product', 'category_id',
             'product_category', 'id');
         Yii::$app->db->createCommand('SET foreign_key_checks = 1')->execute();
