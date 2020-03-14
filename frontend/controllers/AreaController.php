501d2b13e1fa211aaab7bb52476790ebfdd4ea4f
Maxim Gabidullin <after@ya.ru>
2020-03-14 18:07:05 +0300

    merge with old files

--- frontend/controllers/AreaController.php
+++ frontend/controllers/AreaController.php
@@ -4,6 +4,8 @@ namespace frontend\controllers;
 
 use common\models\Area;
 use common\vg\controllers\FrontendController;
+use vsetigoroda\Banner;
+use vsetigoroda\billing\TariffFree;
 
 class AreaController extends FrontendController
 {
@@ -14,6 +16,14 @@ class AreaController extends FrontendController
         return $this->render('index', [
             'area' => $area,
         ]);
+    }
+
+    public function actionDdd()
+    {
+        new Banner();
+
+        $tariffFree = new TariffFree;
+        echo '<pre>'; print_r($tariffFree); echo '</pre>'; die;
     }
 
 }
